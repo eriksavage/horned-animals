9d809c130c95283a2311c78d49f2282ef5f488d8
erik savage <erik.k.savage@gmail.com>
2021-11-29 16:58:41 -0800

    adds Footer component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './Header.js';
+import Footer from './Footer.js';
 
 export default class App extends Component {
   render() {
@@ -7,6 +8,7 @@ export default class App extends Component {
       <div>
         <p>Connected</p>
         <Header />
+        <Footer />
       </div>
     )
   }
