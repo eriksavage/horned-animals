283a35396a78d6489b7e298180c6778ae8656686
erik savage <erik.k.savage@gmail.com>
2021-12-01 15:06:54 -0800

    adds Modal shell structure to SelectedBeast

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -1,9 +1,34 @@
 import { Component } from "react";
+import Modal from 'react-bootstrap/Modal';
+
 
 class SelectedBeast extends Component{
     render(){
         return(
-            <p>test</p>
+            <Modal
+            // show={show}
+            // onHide={handleClose}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Modal heading
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Centered Modal</h4>
+              <p>
+                Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                consectetur ac, vestibulum at eros.
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              {/* <Button onClick={props.onHide}>Close</Button> */}
+            </Modal.Footer>
+          </Modal>
         );
     }
 }
