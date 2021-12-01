f2af0af2b1ec2c1c12c86506c439cfc43d916970
erik savage <erik.k.savage@gmail.com>
2021-11-30 16:44:45 -0800

    adds constructor and imageClick() function to tract favorites

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,6 +1,17 @@
 import { Component } from "react";
 
 class HornedBeast extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            beastFavs: 0
+        }
+    }
+
+    imgClick = () => {
+        this.setState({beastFavs: this.state.beastFavs + 1});
+    }
+
     render() {
         return(
             <>
@@ -9,7 +20,9 @@ class HornedBeast extends Component {
                     src={this.props.imageUrl} 
                     alt={this.props.description} 
                     title={this.props.title}
+                    onClick={this.imgClick}
                 ></img>
+                <p>🫀's: {this.state.beastFavs}</p>
                 <p>{this.props.description}</p>
             </>
         )
